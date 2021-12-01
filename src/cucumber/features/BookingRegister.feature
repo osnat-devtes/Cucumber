fa1f740d3293d69ba76d3e8d30b0a0a585307238
osnat-devtes <osnat80091@gmail.com>
2021-12-01 18:35:22 +0000

    Cucumber_Task3_Parametrization

--- src/cucumber/features/BookingRegister.feature
+++ src/cucumber/features/BookingRegister.feature
@@ -1,6 +1,14 @@
 Feature: Booking register
 
-  Scenario: user can create an account
+  Scenario Outline: user can create an account
     Given an open browser with HotelTestlabCoderslab
-    When a form is filled in with the user's data
+    When the form is filled with email <eMail> first name <firstName> last name <lastName> password <password>
     Then the webpage should contain 'Welcome to your account. Here you can manage all of your personal information and orders.'
+    And close the browser
+
+    Examples:
+    |eMail                  |firstName |lastName|password |
+    |helena.kowalska12@op.pl|Helena    |Kowalska|akslawok |
+    |olek.nowak312@gmail.com|Aleksander|Nowak   |kawonkelO|
+    |joanna.parnas12@ukr.net|Joanna    |Parnas  |SanraP10 |
+    |marek.bajorek0912@op.pl|Marek     |Bajorek |Kerojab87|
--- src/test/java/StepDefinitions/RegisterSteps.java
+++ src/test/java/StepDefinitions/RegisterSteps.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -19,29 +20,34 @@ public class RegisterSteps {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.get("https://hotel-testlab.coderslab.pl/");
-
     }
 
-    @When("a form is filled in with the user's data")
-    public void aFormIsFilledInWithTheUserSData() {
+    @When("^the form is filled with email (.*) first name (.*) last name (.*) password (.*)$")
+    public void theFormIsFilledWithData(String eMail, String firstName, String lastName, String password) {
         WebElement signIn = driver.findElement(By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[7]/ul/li/a/span"));
         signIn.click();
-        WebElement eMail = driver.findElement(By.xpath("//*[@id=\"email_create\"]"));
-        eMail.sendKeys("hjkhkjh12@op.pl");
+        WebElement eMailInput = driver.findElement(By.xpath("//*[@id=\"email_create\"]"));
+        eMailInput.sendKeys(eMail);
         WebElement createAnAccountButton = driver.findElement(By.xpath("//*[@id=\"SubmitCreate\"]/span"));
         createAnAccountButton.click();
         WebElement firstNameInput = driver.findElement(By.xpath("//*[@id=\"customer_firstname\"]"));
-        firstNameInput.sendKeys("Jan");
+        firstNameInput.sendKeys(firstName);
         WebElement lastNameInput = driver.findElement(By.xpath("//*[@id=\"customer_lastname\"]"));
-        lastNameInput.sendKeys("Kowalski");
+        lastNameInput.sendKeys(lastName);
         WebElement passwordInput = driver.findElement(By.xpath("//*[@id=\"passwd\"]"));
-        passwordInput.sendKeys("QWERTY12");
+        passwordInput.sendKeys(password);
         WebElement registerButton = driver.findElement(By.xpath("//*[@id=\"submitAccount\"]"));
         registerButton.click();
+
     }
 
     @Then("the webpage should contain {string}")
-    public void theWebpageShouldContainWelcomeToYourAccountHereYouCanManageAllOfYourPersonalInformationAndOrders(String expectedString){
+    public void theWebpageShouldContainWelcomeToYourAccount(String expectedString){
         System.out.println(expectedString);
+    }
+
+    @And("close the browser")
+    public void closeTheBrowser() {
+        driver.quit();
     }
 }
